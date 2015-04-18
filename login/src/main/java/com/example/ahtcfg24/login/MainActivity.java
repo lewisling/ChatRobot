f9809a36302b52bad18e4e7a164d4d01da198c53
xuding <ahtcfg24@mail.bistu.edu.cn>
2015-04-19 02:13:41 +0800

    back to inner class.

--- login/src/main/java/com/example/ahtcfg24/login/MainActivity.java
+++ login/src/main/java/com/example/ahtcfg24/login/MainActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class MainActivity extends Activity
@@ -20,18 +22,31 @@ public class MainActivity extends Activity
         final CheckBox checkBox_show = (CheckBox) findViewById(R.id.show_password);
         final EditText edit_password = (EditText) findViewById(R.id.edit_password);
 
-        checkBox_show.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (checkBox_show.isChecked())
+        checkBox_show.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
             {
-                edit_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-            } else
-            {
-                edit_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                if (checkBox_show.isChecked())
+                {
+                    edit_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else
+                {
+                    edit_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
             }
         });
 
         Button button_about = (Button) findViewById(R.id.button_about);
 
-        button_about.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AboutActivity.class)));
+        button_about.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /*about the "this" ,I used it in the inner class,so I have to add the MainActivity*/
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            }
+        });
     }
 }
