6e8fd1cb6e714dc0dcb73fbcd3a8085949c8433f
xuding <ahtcfg24@mail.bistu.edu.cn>
2015-04-18 14:53:56 +0800

    changed the image of head and adopt the lambda.

--- login/src/main/java/com/example/ahtcfg24/login/MainActivity.java
+++ login/src/main/java/com/example/ahtcfg24/login/MainActivity.java
@@ -5,11 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class MainActivity extends Activity
@@ -19,34 +16,22 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*匿名内部类访问外部类的变量时，需要给外部变量声明为final型*/
+        /*鍖垮悕鍐呴儴绫昏闂閮ㄥ彉閲忥紝澶栭儴鍙橀噺瑕佺敤final*/
         final CheckBox checkBox_show = (CheckBox) findViewById(R.id.show_password);
         final EditText edit_password = (EditText) findViewById(R.id.edit_password);
 
-        checkBox_show.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+        checkBox_show.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (checkBox_show.isChecked())
             {
-                if (checkBox_show.isChecked())
-                {
-                    edit_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                } else
-                {
-                    edit_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                }
+                edit_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            } else
+            {
+                edit_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
             }
         });
 
         Button button_about = (Button) findViewById(R.id.button_about);
 
-        button_about.setOnClickListener(new OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                startActivity(new Intent(MainActivity.this, AboutActivity.class));
-            }
-        });
+        button_about.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AboutActivity.class)));
     }
 }
