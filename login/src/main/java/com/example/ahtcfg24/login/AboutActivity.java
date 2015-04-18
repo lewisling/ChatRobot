c88b82f5ad9e794dea40ca0509a22bc9ddbcc758
xuding <ahtcfg24@mail.bistu.edu.cn>
2015-04-18 13:35:08 +0800

    modifi the ArrayList.

---
+++ login/src/main/java/com/example/ahtcfg24/login/AboutActivity.java
@@ -0,0 +1,91 @@
+package com.example.ahtcfg24.login;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by ahtcfg24 on 2015/3/3.
+ * register activity
+ */
+public class AboutActivity extends ListActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+        ArrayList<HashMap<String, String>> about_list = new ArrayList<>();//创建一个ArrayList的对象
+        HashMap<String, String> map1 = new HashMap<>();
+        HashMap<String, String> map2 = new HashMap<>();
+        HashMap<String, String> map3 = new HashMap<>();
+        HashMap<String, String> map4 = new HashMap<>();
+        HashMap<String, String> map5 = new HashMap<>();
+        HashMap<String, String> map6 = new HashMap<>();
+        HashMap<String, String> map7 = new HashMap<>();
+        HashMap<String, String> map8 = new HashMap<>();
+        map1.put("title", "开  发  者：");
+        map1.put("value", "徐鼎");
+        map2.put("title", "日        期：");//安卓中一个汉字等于四个空格的大小
+        map2.put("value", "2015/3/8");
+        map3.put("title", "版        本：");
+        map3.put("value", "1.01");
+        map4.put("title", "邮        箱：");
+        map4.put("value", "ahtcfg24@163.com");
+        map5.put("title", "抠  抠  号：");
+        map5.put("value", "896433809");
+        map6.put("title", "手  机  号：");
+        map6.put("value", "15600909030");
+        map7.put("title", "微  信  号：");
+        map7.put("value", "ahtcfg24");
+        map8.put("title", "新浪微博：");
+        map8.put("value", "绝世盗草人");
+        about_list.add(map1);
+        about_list.add(map2);
+        about_list.add(map3);
+        about_list.add(map4);
+        about_list.add(map5);
+        about_list.add(map6);
+        about_list.add(map7);
+        about_list.add(map8);
+        SimpleAdapter simpleAdapter = new SimpleAdapter(
+                this,
+                about_list,
+                R.layout.about_list,
+                new String[]{"title", "value"},
+                new int[]{R.id.list_title, R.id.list_value});
+        setListAdapter(simpleAdapter);
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        System.out.println("" + id + " " + position);
+        if (position == 7)
+            toWeb("http://m.weibo.cn/u/3180900562?jumpfrom=weibocom");
+        if(position==5)
+            toDial("tel:15600909030");
+
+    }
+    public void toDial(String num){
+        Intent ToDial=new Intent();
+        ToDial.setAction(Intent.ACTION_DIAL);
+        ToDial.setData(Uri.parse(num));
+        startActivity(ToDial);
+    }
+
+    public void toWeb(String address) {
+        Intent ToWeb = new Intent(Intent.ACTION_VIEW);
+        ToWeb.setData(Uri.parse(address));
+        ToWeb = Intent.createChooser(ToWeb, null);
+        startActivity(ToWeb);
+    }
+}
+
