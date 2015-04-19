361154fb7f6d960d4d5ef38c96a0c26b43985fad
xuding <ahtcfg24@mail.bistu.edu.cn>
2015-04-19 11:27:11 +0800

    Added the init() and finished().I have implemented the function of welcome.

--- isfirstrun/src/main/java/com/example/ahtcfg24/isfirstrun/ActivityWelcome.java
+++ isfirstrun/src/main/java/com/example/ahtcfg24/isfirstrun/ActivityWelcome.java
@@ -2,6 +2,7 @@ package com.example.ahtcfg24.isfirstrun;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +13,13 @@ public class ActivityWelcome extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        /*The first param is a String key of the file's name.If the file is not exist,it will be created.
+        * the second param is a int mode ,it stands for a mode of the file's write-read way.*/
+        SharedPreferences mySharedPreferences = getSharedPreferences("fileName", MODE_PRIVATE);
+
+        init(mySharedPreferences);
+        /*Destroy the welcome activity*/
+        finish();
     }
 
     /**
@@ -26,5 +34,29 @@ public class ActivityWelcome extends Activity
     public void goGuide(View view)
     {
         startActivity(new Intent(this, Activity_Guide.class));
+    }
+
+    /**
+     * performing during the stage of initiation.Made a decision about which activity will be started.
+     *
+     * @param mySharedPreferences deliver a SharedPreference file object.
+     */
+    public void init(SharedPreferences mySharedPreferences)
+    {
+        /*Get the value of the key :isFirstRun.
+        If the isFirstRun's value is not exists,it will be set to true.*/
+        if (mySharedPreferences.getBoolean("isFirstRun", true))
+        {
+            startActivity(new Intent(this, Activity_Guide.class));
+            /*The edit() method will return a interface object,so we evaluate it to myEditor.*/
+            SharedPreferences.Editor myEditor = mySharedPreferences.edit();
+            /*Modified the value to false by calling the putBoolean*/
+            myEditor.putBoolean("isFirstRun", false);
+            /*After modification,it must be save.*/
+            myEditor.apply();
+        } else
+        {
+            startActivity(new Intent(this, Activity_Main.class));
+        }
     }
 }
