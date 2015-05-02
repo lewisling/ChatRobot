5bef85efee4dc1ce28ee292c1727783ebce968b7
xuding <ahtcfg24@mail.bistu.edu.cn>
2015-05-03 01:52:56 +0800

    merge
    merge the login and isfirstrun

---
+++ BayMax/src/main/java/com/example/ahtcfg24/baymax/ActivityGuide.java
@@ -0,0 +1,100 @@
+package com.example.ahtcfg24.baymax;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityGuide extends Activity
+{
+    private List<View> view_list;
+    private int currentIndex;
+    private ViewPager myViewPager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+        initPage();
+        initDots();
+    }
+
+    public void initPage()
+    {
+        /*find the view of ViewPager*/
+        myViewPager = (ViewPager) findViewById(R.id.guide_viewpager);
+        /*the list is used to save pages.*/
+        view_list = new ArrayList<>();
+        /*initial the page resource*/
+        int[] resource = {R.layout.guide_page1, R.layout.guide_page2, R.layout.guide_page3};
+        /*inflate the pages into the list.*/
+        inflatePage(resource);
+        /*fill the viewpager*/
+        myViewPager.setAdapter(new AdapterGuide(view_list));
+    }
+
+    public void initDots()
+    {
+        /*find dots*/
+        LinearLayout dot_layout = (LinearLayout) findViewById(R.id.my_layout);
+        final ImageView[] dots = new ImageView[3];
+        for (int i = 0; i < 3; i++)
+        {
+            dots[i] = (ImageView) dot_layout.getChildAt(i);
+            dots[i].setEnabled(true);
+        }
+        //set first dot on highlight
+        dots[0].setEnabled(false);
+        //set listener
+        myViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener()
+        {
+            /**
+             * called when the new page is selected.
+             * @param position the number of new page
+             */
+            @Override
+            public void onPageSelected(int position)
+            {
+                dots[position].setEnabled(false);
+                dots[currentIndex].setEnabled(true);
+                currentIndex = position;
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+            {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state)
+            {
+
+            }
+        });
+    }
+
+    public void inflatePage(int[] resource)
+    {
+        /*get a LayoutInflater object from default context*/
+        LayoutInflater inflater = getLayoutInflater();
+        for (int aResource : resource)
+        {
+            view_list.add(inflater.inflate(aResource, null));
+        }
+    }
+
+    public void goMain(View view)
+    {
+        startActivity(new Intent(ActivityGuide.this, ActivityMain.class));
+        ActivityGuide.this.finish();
+    }
+}
---
+++ BayMax/src/main/java/com/example/ahtcfg24/baymax/ActivityMain.java
@@ -0,0 +1,63 @@
+
+package com.example.ahtcfg24.baymax;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+/**
+ * <p>Description: </p>
+ *
+ * @author XuDing
+ * @version 1.0
+ * @date 2015/5/3
+ */
+
+
+
+public class ActivityMain extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*When the inner class want to access to the outer variable，the variable need to be added final.*/
+        final CheckBox checkBox_show = (CheckBox) findViewById(R.id.show_password);
+        final EditText edit_password = (EditText) findViewById(R.id.edit_password);
+
+        checkBox_show.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if (checkBox_show.isChecked())
+                {
+                    edit_password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else
+                {
+                    edit_password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+        Button button_about = (Button) findViewById(R.id.button_about);
+
+        button_about.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /*about the "this" ,I used it in the inner class,so I have to add the MainActivity*/
+                startActivity(new Intent(ActivityMain.this, ActivityAbout.class));
+            }
+        });
+    }
+}
